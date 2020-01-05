39a5e0163543351214d79d8534d7df6d46e518ca
Dup4 <panlvzhi@foxmail.com>
2020-01-05 16:38:44 +0800

    up

--- web/OJ/include/static.php
+++ web/OJ/include/static.php
@@ -6,14 +6,14 @@
 ?>
 <?php  
 
-  static  $DB_HOST="127.0.0.1";
+  static  $DB_HOST="172.22.237.6";
   static  $DB_VJHOST="127.0.0.1";
   static  $DB_NAME="jol";
   static  $DB_VJNAME="vhoj";
   static  $DB_USER="root";
   static  $DB_VJUSER="root";
   //static  $DB_PASS="root";
-  static  $DB_PASS="root";
+  static  $DB_PASS="hznuonlinejudge!!!";
   static  $DB_VJPASS="root"; 
 
   $OJ_HOME=dirname(dirname(__FILE__));//:OJ/
