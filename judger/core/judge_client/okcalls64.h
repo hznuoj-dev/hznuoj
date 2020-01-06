928482305e212d75c02fc96d7b8d1e0f2b0426d6
Dup4 <panlvzhi@foxmail.com>
2020-01-06 10:31:34 +0800

    up okcalls

--- judger/core/judge_client/okcalls64.h
+++ judger/core/judge_client/okcalls64.h
@@ -18,44 +18,48 @@
  */
 
 //c & c++
-int LANG_CV[1024] = {0,1,2,3,4,5,8,9,11,12,14,20,21,59,63,89,99,158,186,231,234,240, SYS_time, SYS_read, SYS_uname, SYS_write, SYS_open,
+int LANG_CV[1024] = {0,1,2,3,4,5,8,9,11,12,14,20,21,59,63,89,99,158,186,231,234,240,252,
+		SYS_time, SYS_read, SYS_uname, SYS_write, SYS_open,
 		SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
-		SYS_mmap, SYS_fstat, SYS_set_thread_area, 252, SYS_arch_prctl, 0 };
+		SYS_mmap, SYS_fstat, SYS_set_thread_area,SYS_arch_prctl, 0 };
 //pascal
-int LANG_PV[1024] = {0,1,2,3,4,9,11,13,16,59,89,97,201,231,SYS_open, SYS_set_thread_area, SYS_brk, SYS_read,
+int LANG_PV[1024] = {0,1,2,3,4,9,11,13,16,59,89,97,158,191,201,231,252,
+		SYS_open, SYS_set_thread_area, SYS_brk, SYS_read,
 		SYS_uname, SYS_write, SYS_execve, SYS_ioctl, SYS_readlink, SYS_mmap,
-		SYS_rt_sigaction, SYS_getrlimit, 252, 191, 158, 231, SYS_close,
-		SYS_exit_group, SYS_munmap, SYS_time, 4, 0 };
+		SYS_rt_sigaction, SYS_getrlimit,SYS_close,
+		SYS_exit_group, SYS_munmap, SYS_time,0 };
 //java
-int LANG_JV[1024] = { 0,2,3,4,5,9,10,11,12,13,14,21,56,59,89,97,104,158,202,218,231,273,257,302, 
-		61, 22, 6, 33, 8, 13, 16, 111, 110, 39, 79, SYS_fcntl,
+int LANG_JV[1024] = { 0,2,3,4,5,6,8,9,10,11,12,13,14,16,21,22,33,39,56,59,61,79,89,97,104,
+		110,111,158,202,218,231,273,257,302, 
+		SYS_fcntl,
 		SYS_getdents64, SYS_getrlimit, SYS_rt_sigprocmask, SYS_futex, SYS_read,
 		SYS_mmap, SYS_stat, SYS_open, SYS_close, SYS_execve, SYS_access,
 		SYS_brk, SYS_readlink, SYS_munmap, SYS_close, SYS_uname, SYS_clone,
 		SYS_uname, SYS_mprotect, SYS_rt_sigaction, SYS_getrlimit, SYS_fstat,
 		SYS_getuid, SYS_getgid, SYS_geteuid, SYS_getegid, SYS_set_thread_area,
-		SYS_set_tid_address, SYS_set_robust_list, SYS_exit_group, 158, 0 };
+		SYS_set_tid_address, SYS_set_robust_list, SYS_exit_group, 0 };
 //ruby
-int LANG_RV[1024] = { 0,1,2,3,4,5,6,9,10,12,13,14,16,20,21,22,56,59,72,97,98,107,108,131,158,202,218,228,231,273,293,318
-		,96, 340, 4, 126,79, 302,SYS_access, SYS_arch_prctl, SYS_brk,
+int LANG_RV[1024] = { 0,1,2,3,4,5,6,9,10,12,13,14,16,20,21,22,56,59,72,79,96,97,98,107,
+		108,126,131,158,202,218,228,231,273,293,302,318,340,
+		SYS_access, SYS_arch_prctl, SYS_brk,
 		SYS_close, SYS_execve, SYS_exit_group, SYS_fstat, SYS_futex,
 		SYS_getegid, SYS_geteuid, SYS_getgid, SYS_getuid, SYS_getrlimit,
 		SYS_mmap, SYS_mprotect, SYS_munmap, SYS_open, SYS_read,
 		SYS_rt_sigaction, SYS_rt_sigprocmask, SYS_set_robust_list,
 		SYS_set_tid_address, SYS_write, 0 };
 //bash
-int LANG_BV[1024] = { 0,1,2,3,4,5,8,9,10,12,13,14,16,21,33,39,59,63,72,79,97,99,102,104,107,108,110,111,158,231,
-		96, 22, 61, 56, 42, 41, 79, 158, 117, 60, 39, 102, 191,302,
-		183, SYS_access, SYS_arch_prctl, SYS_brk, SYS_close, SYS_dup2,
+int LANG_BV[1024] = { 0,1,2,3,4,5,8,9,10,12,13,14,16,21,22,33,39,41,42,56,59,
+		60,61,63,72,79,96,97,99,102,104,107,108,110,111,117,158,183,191,231,302,            
+		SYS_access, SYS_arch_prctl, SYS_brk, SYS_close, SYS_dup2,
 		SYS_execve, SYS_exit_group, SYS_fcntl, SYS_fstat, SYS_getegid,
 		SYS_geteuid, SYS_getgid, SYS_getpgrp, SYS_getpid, SYS_getppid,
 		SYS_getrlimit, SYS_getuid, SYS_ioctl, SYS_lseek, SYS_mmap, SYS_mprotect,
 		SYS_munmap, SYS_open, SYS_read, SYS_rt_sigaction, SYS_rt_sigprocmask,
-		SYS_stat, SYS_uname, SYS_write, 14, 0 };
+		SYS_stat, SYS_uname, SYS_write,  0 };
 //python
-int LANG_YV[1024] = {0,1,2,3,4,5,6,8,9,10,12,13,14,16,20,21,59,78,79,89,97,99,102,104,107,108,158,218,231,273,32,72,
-		131,1,2,3,4,5,6,8,9,10,11,12,13,14,16,21,59,78,79,89,97,102,104,107,108,158,202,218,231,257,273,318,41, 42,
-		146, SYS_mremap, 158, 117, 60, 39, 102, 191,302,
+int LANG_YV[1024] = {0,1,2,3,4,5,6,8,9,10,11,12,13,14,16,20,21,32,39,41,42,59,60,72,78,79,
+		89,97,99,102,104,107,108,117,131,146,158,191,202,218,231,257,273,302,318,
+		SYS_mremap,      
 		SYS_access, SYS_arch_prctl, SYS_brk, SYS_close, SYS_execve,
 		SYS_exit_group, SYS_fcntl, SYS_fstat, SYS_futex, SYS_getcwd,
 		SYS_getdents, SYS_getegid, SYS_geteuid, SYS_getgid, SYS_getrlimit,
@@ -75,8 +79,9 @@ int LANG_PHV[1024] = { 0,1,2,3,4,5,6,8,9,10,11,12,13,14,16,20,21,28,39,59,60,78,
 		SYS_set_thread_area, SYS_set_tid_address, SYS_stat, SYS_time, SYS_uname,
 		SYS_write, 0 };
 //perl
-int LANG_PLV[1024] = {0,1,2,3,4,5,8,9,10,12,13,14,16,21,59,72,89,97,102,104,107,108,158,202,218,231,273,
-		 96, 78, 158, 117, 60, 39, 102, 191, 302,SYS_access, SYS_brk,
+int LANG_PLV[1024] = {0,1,2,3,4,5,8,9,10,12,13,14,16,21,39,59,60,72,78,89,96,97,102,104,107,108,117,
+		158,191,202,218,231,273,302,
+		SYS_access, SYS_brk,
 		SYS_close, SYS_execve, SYS_exit_group, SYS_fcntl, SYS_fstat, SYS_futex,
 		SYS_getegid, SYS_geteuid, SYS_getgid, SYS_getrlimit, SYS_getuid,
 		SYS_ioctl, SYS_lseek, SYS_mmap, SYS_mprotect, SYS_munmap, SYS_open,
@@ -85,10 +90,9 @@ int LANG_PLV[1024] = {0,1,2,3,4,5,8,9,10,12,13,14,16,21,59,72,89,97,102,104,107,
 		SYS_time, SYS_uname, SYS_write, 0 };
 
 //c-sharp
-int LANG_CSV[1024] = {0,1,2,3,4,5,8,9,10,11,12,13,14,16,21,24,41,42,56,59,63,72,78,79,89,97
-		,102,131,137,157,158,202,204,218,229,231,234,257,273, 257, 141, 95, 64, 65, 66
-		, 83, 24, 42, 41, 158, 117, 60,302,
-		39, 102, 191, SYS_access, SYS_brk, SYS_chmod, SYS_clock_getres,
+int LANG_CSV[1024] = {0,1,2,3,4,5,8,9,10,11,12,13,14,16,21,24,39,41,42,56,59,60,63,64,65,66,72,78,79,83,89,95,97,
+		102,117,131,137,141,157,158,191,202,204,218,229,231,234,257,273,257,302,
+		SYS_access, SYS_brk, SYS_chmod, SYS_clock_getres,
 		SYS_clock_gettime, SYS_clone, SYS_close, SYS_execve, SYS_exit_group,
 		SYS_fcntl, SYS_fstat, SYS_ftruncate, SYS_futex, SYS_getcwd,
 		SYS_getdents, SYS_geteuid, SYS_getpid, SYS_getppid, SYS_getrlimit,
@@ -100,7 +104,7 @@ int LANG_CSV[1024] = {0,1,2,3,4,5,8,9,10,11,12,13,14,16,21,24,41,42,56,59,63,72,
 		SYS_set_thread_area, SYS_set_tid_address, SYS_sigaltstack, SYS_stat,
 		SYS_statfs, SYS_tgkill, SYS_time, SYS_uname, SYS_unlink, SYS_write, 0 };
 //objective-c
-int LANG_OV[1024] = { 0,1,2,3,4,5,9,10,12,21,59,158,231, 102, 191, SYS_access, SYS_brk, SYS_close,
+int LANG_OV[1024] = { 0,1,2,3,4,5,9,10,12,21,59,158,231,102,191, SYS_access, SYS_brk, SYS_close,
 		SYS_execve, SYS_exit_group, SYS_fstat, SYS_futex, SYS_getcwd,
 		SYS_getrlimit, SYS_gettimeofday, SYS_mmap, SYS_mprotect, SYS_munmap,
 		SYS_open, SYS_read, SYS_readlink, SYS_rt_sigaction, SYS_rt_sigprocmask,
@@ -115,7 +119,7 @@ int LANG_BASICV[1024] = { 0,1,2,3,4,5,9,10,12,13,14,16,21,59,97,158,173,202,218,
 		SYS_rt_sigprocmask, SYS_set_robust_list, SYS_set_thread_area,
 		SYS_set_tid_address, SYS_stat, SYS_uname, SYS_write, 0 };
 //scheme
-int LANG_SV[1024] = { 0,1,2,3,4,5,8,9,10,11,12,13,14,16,21,56,59,72,97,158,202,204,218,228,229,234,273,293,0,
+int LANG_SV[1024] = { 0,1,2,3,4,5,8,9,10,11,12,13,14,16,21,56,59,72,97,158,202,204,218,228,229,234,273,293,
 		SYS_fcntl, SYS_getdents64, SYS_getrlimit, SYS_rt_sigprocmask, SYS_futex,
 		SYS_read, SYS_mmap, SYS_stat, SYS_open, SYS_close, SYS_execve,
 		SYS_access, SYS_brk, SYS_readlink, SYS_munmap, SYS_close, SYS_uname,
@@ -128,4 +132,4 @@ int LANG_LUAV[1024]={0,1,2,3,4,5,9,10,11,12,13,21,59,158,231,292,0};
 //nodejs javascript
 int LANG_JSV[1024]={0,1,2,3,4,5,6,9,10,11,12,13,14,16,21,56,59,79,89,96,97,158,186,202,218,228,231,232,233,273,290,291,293,0};
 //go-lang
-int LANG_GOV[1024]={0,1,9,11,13,14,56,59,131,158,186,202,204,231,0};
+int LANG_GOV[1024]={0,1,9,11,13,14,56,59,131,158,186,202,204,231,267,0};
