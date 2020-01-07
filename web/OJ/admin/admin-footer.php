8fa65a6912831b60b012a524a7e2c7d82caa929c
Dup4 <panlvzhi@foxmail.com>
2020-01-07 11:59:42 +0800

    up

--- web/OJ/admin/admin-footer.php
+++ web/OJ/admin/admin-footer.php
@@ -1,4 +1,6 @@
 
+
+
 <!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
 <script src="/OJ/plugins/jquery/jquery-3.1.1.min.js"></script>
 
@@ -6,7 +8,8 @@
 <script src="/OJ/plugins/bootstrap/js/bootstrap.min.js"></script>
 <script src="/OJ/plugins/bootstrap/js/bootstrap-filestyle.min.js"></script>
 <script src="/OJ/plugins/bootstrap/js/bootstrap-select.min.js"></script>
-</div><!-- container -->
+</div>
+<!-- container -->
 
 </body>
 </html>
