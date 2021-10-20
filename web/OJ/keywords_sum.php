502ff9e46c3c70ab7470004c49c9ad5ea1602e63
fyhkimyon <1292346917@qq.com>
2021-10-20 06:08:51 -0700

    fix XSS bug

--- web/OJ/keywords_sum.php
+++ web/OJ/keywords_sum.php
@@ -9,6 +9,11 @@ require_once("./include/my_func.inc.php");
 $view_title= "Source Code"; 
 
 /////////////////////////Template
+if (!isset($_SESSION['user_id'])){
+    $view_errors= "<a href=./loginpage.php>$MSG_Login</a>";
+    require("template/".$OJ_TEMPLATE."/error.php");
+    exit(0);
+  }
 include_once "keywords_sum_show.php";
 /////////////////////////Common foot
 if(file_exists('./include/cache_end.php'))
