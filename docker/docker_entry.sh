fe7e97079952f5c272b160e4deeacc5ac6f01283
Dup4 <lyuzhi.pan@gmail.com>
2021-10-23 15:13:32 +0000

    feat: docker support for webapp

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -e -x
-chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-service mysql restart
-service apache2 restart
-judged
 
-if [ -z "$@" ]; then
+set -e -x
+service apache2 restart
+
+if [ -z "$*" ]; then
 	/bin/bash
 else
 	exec "$@"
