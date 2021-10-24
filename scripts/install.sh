2efd8b3720a125ddba8a5d88b516f0ebb3f268ac
Dup4 <lyuzhi.pan@gmail.com>
2021-10-24 10:53:48 +0000

    fix: apache2 prefork configuration replace

--- scripts/install.sh
+++ scripts/install.sh
@@ -39,4 +39,4 @@ sed -i -e 's/\/var\/www\/html/\/var\/www\/web/g' /etc/apache2/sites-available/00
 sed -i -e 's/Options Indexes FollowSymLinks/Options FollowSymLinks/g' /etc/apache2/apache2.conf
 
 # set limit of prefork mode
-cat "${TOP_DIR}/etc/apache2/mods-available/mpm_prefork.conf" >/etc/apache2/mods-available/mpm_prefork.conf
+cat "${TOP_DIR}/../etc/apache2/mods-available/mpm_prefork.conf" >/etc/apache2/mods-available/mpm_prefork.conf
