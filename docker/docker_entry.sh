3753f4bf22be1f94ddcee711742582c76667bdb0
Dup4 <lyuzhi.pan@gmail.com>
2021-10-25 21:42:18 +0800

    Update docker_entry.sh

--- docker/docker_entry.sh
+++ docker/docker_entry.sh
@@ -2,6 +2,14 @@
 
 set -e -x
 
+WEBBASE=/var/www
+APACHEUSER=www-data
+UPLOAD=${WEBBASE}/web/OJ/upload
+DATA=/var/hznuoj/data
+
+chown -R ${APACHEUSER} ${DATA}
+chown -R ${APACHEUSER} ${UPLOAD}
+
 service apache2 restart
 
 if [ -z "$*" ]; then
