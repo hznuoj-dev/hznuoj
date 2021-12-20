01b4afb354250236fccf6bbc935b5c0639f94dfd
Dup4 <lyuzhi.pan@gmail.com>
2021-12-20 21:31:25 +0800

    feat: add configure script

---
+++ docker/configure.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+for script in /scripts/bin/*; do
+    if [ -f "$script" ]; then
+        chmod 755 "$script"
+        ln -s "$script" /usr/bin/
+    fi
+done
+
+# Configure php
+php_folder=$(echo "/etc/php/7."?"/")
+
+# shellcheck disable=SC2034
+php_version=$(basename "$php_folder")
+
+if [ ! -d "$php_folder" ]; then
+    echo "[!!] Could not find php path"
+    exit 1
+fi
+
+# Set correct settings
+sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = 512M/" \
+    -e "s/^post_max_size.*/post_max_size = 256M/" \
+    -e "s/^memory_limit.*/memory_limit = 2G/" \
+    -e "s/^max_file_uploads.*/max_file_uploads = 200/" \
+    -e "s#^;date\.timezone.*#date.timezone = ${CONTAINER_TIMEZONE}#" \
+    "$php_folder/fpm/php.ini"
