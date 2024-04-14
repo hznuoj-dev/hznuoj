3eb2e7c1014d3c1013a668a591708bbdf6d01e96
daizeyao <1192313493@qq.com>
2024-04-14 11:34:28 +0800

    Update maintainer-list.php

--- web/OJ/maintainer-list.php
+++ web/OJ/maintainer-list.php
@@ -1,227 +1,141 @@
+<?php $title = "Maintainer List"; ?>
 <?php include "template/hznu/header.php" ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>HZNUOJ -- Maintainer List</title>
     <style>
-        .box{
+        .box {
             border: 1px solid #eee;
             padding: 30px;
             margin: 25px 0 15px 0;
             box-shadow: 2px 2px 10px 0 #ccc;
         }
+
         .am-table tr:first-child td {
             border-top: 0;
         }
+
         .am-table {
             margin-bottom: 0;
         }
     </style>
 </head>
+
 <body>
     <div class="am-container" style="padding-top: 20px; max-width: 800px;">
         <h1>Maintainer History</h1>
         <hr>
-        <div class="box">
-            <table class="am-table">
-                <tbody>
-                    <tr>
-                        <td width="20%">Name</td>
-                        <td>Lyuzhi Pan <span class="am-badge am-badge-success">Active</span></td>
-                    </tr>
-                    <tr>
-                        <td>School Year</td>
-                        <td>2017</td>
-                    </tr>
-                    <tr>
-                        <td>Sex</td>
-                        <td>Male</td>
-                    </tr>
-                    <tr>
-                        <td>Nickname</td>
-                        <td>Dup4</td>
-                    </tr>
-                    <tr>
-                        <td>E-Mail</td>
-                        <td><a href="mailto:panlvzhi@foxmail.com">panlvzhi@foxmail.com</a></td>
-                    </tr>
-                    <tr>
-                        <td>Blog</td>
-                        <td>
-                            <a href="https://github.com/Dup4">github.com/Dup4</a><br>
-                            <a href="https://dup4.cn">dup4.cn</a><br>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Motto</td>
-                        <td>
-                            <p>NULL.</p>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="box">
-            <table class="am-table">
-                <tbody>
-                    <tr>
-                        <td width="20%">Name</td>
-                        <td>Kaiqing Zhang <span class="am-badge am-badge-success">Active</span></td>
-                    </tr>
-                    <tr>
-                        <td>School Year</td>
-                        <td>2016</td>
-                    </tr>
-                    <tr>
-                        <td>Sex</td>
-                        <td>Male</td>
-                    </tr>
-                    <tr>
-                        <td>Nickname</td>
-                        <td>ZKin</td>
-                    </tr>
-                    <tr>
-                        <td>E-Mail</td>
-                        <td><a href="mailto:zkqhnfg@gmail.com">zkqhnfg@gmail.com</a></td>
-                    </tr>
-                    <tr>
-                        <td>Blog</td>
-                        <td>
-                            <a href="https://github.com/ZKingQ">github.com/ZKingQ</a><br>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Motto</td>
-                        <td>
-                            <p>Make the world a better place</p>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="box">
-            <table class="am-table">
-                <tbody>
-                <tr>
-                    <td width="20%">Name</td>
-                    <td>Lixin Wei <span class="am-badge am-badge-success">Active</span></td>
-                </tr>
-                <tr>
-                    <td>School Year</td>
-                    <td>2015</td>
-                </tr>
-                <tr>
-                    <td>Sex</td>
-                    <td>Male</td>
-                </tr>
-                <tr>
-                    <td>Nickname</td>
-                    <td>D_Star, The_Dawn_Star</td>
-                </tr>
-                <tr>
-                    <td>E-Mail</td>
-                    <td><a href="mailto:wlx65005@gmail.com">wlx65005@gmail.com</a></td>
-                </tr>
-                <tr>
-                    <td>Blog</td>
-                    <td>
-                        <a href="https://github.com/wlx65003">github.com/wlx65003</a><br>
-                        <a href="http://blog.csdn.net/wlx65003">blog.csdn.net/wlx65003</a><br>
-                        <a href="https://d-star.xyz">d-star.xyz</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Motto</td>
-                    <td>
-                        <p>After all, tomorrow is another day.</p>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="box">
-            <table class="am-table">
-                <tbody>
-                <tr>
-                    <td width="20%">Name</td>
-                    <td>Ruiyu Zeng <span class="am-badge">Inactive</span></td>
-                </tr>
-                <tr>
-                    <td>School Year</td>
-                    <td>2014</td>
-                </tr>
-                <tr>
-                    <td>Sex</td>
-                    <td>Male</td>
-                </tr>
-                <tr>
-                    <td>Nickname</td>
-                    <td>hhhh</td>
-                </tr>
-                <tr>
-                    <td>E-Mail</td>
-                    <td><a href="mailto:398585603@qq.com">398585603@qq.com</a></td>
-                </tr>
-                <tr>
-                    <td>Blog</td>
-                    <td>
-                        none
-                    </td>
-                </tr>
-                <tr>
-                    <td>Motto</td>
-                    <td>
-                        <p>none</p>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-        <div class="box">
-            <table class="am-table">
-                <tbody>
-                <tr>
-                    <td width="20%">Name</td>
-                    <td>Yupeng Chen <span class="am-badge">Inactive</span><span class="am-badge am-badge-secondary">The Founder</span></td>
-                </tr>
-                <tr>
-                    <td>School Year</td>
-                    <td>2012</td>
-                </tr>
-                <tr>
-                    <td>Sex</td>
-                    <td>Male</td>
-                </tr>
-                <tr>
-                    <td>Nickname</td>
-                    <td>yybird, yuncity</td>
-                </tr>
-                <tr>
-                    <td>E-Mail</td>
-                    <td><a href="mailto:cyp@hsACM.com">cyp@hsACM.com</a></td>
-                </tr>
-                <tr>
-                    <td>Blog</td>
-                    <td>
-                        <p>none</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Motto</td>
-                    <td>
-                        <p>Who am I? Where am I? Why am I here?</p>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+
+        <?php
+        $people = [
+            [
+                'Name' => 'Zeyao Dai',
+                'Status' => 'Active',
+                'School Year' => '2021',
+                'Sex' => 'Male',
+                'Nickname' => 'Dzy521',
+                'E-Mail' => 'zeyaodai@gmail.com',
+                'Blog' => ['https://github.com/daizeyao'],
+                'Motto' => 'NULL.'
+            ],
+            [
+                'Name' => 'Jiafu Yang',
+                'Status' => 'Active',
+                'School Year' => '2021',
+                'Sex' => 'Male',
+                'Nickname' => 'learner',
+                'E-Mail' => '2733197830@qq.com',
+                'Blog' => ['https://yangjiafu.cn'],
+                'Motto' => 'NULL.'
+            ],
+            [
+                'Name' => 'Lyuzhi Pan',
+                'Status' => 'Inactive',
+                'School Year' => '2017',
+                'Sex' => 'Male',
+                'Nickname' => 'Dup4',
+                'E-Mail' => 'panlvzhi@foxmail.com',
+                'Blog' => ['https://github.com/Dup4', 'https://dup4.cn'],
+                'Motto' => 'NULL.'
+            ],
+            [
+                'Name' => 'Kaiqing Zhang',
+                'Status' => 'Inactive',
+                'School Year' => '2016',
+                'Sex' => 'Male',
+                'Nickname' => 'ZKin',
+                'E-Mail' => 'zkqhnfg@gmail.com',
+                'Blog' => ['https://github.com/ZKingQ'],
+                'Motto' => 'Make the world a better place'
+            ],
+            [
+                'Name' => 'Lixin Wei',
+                'Status' => 'Inactive',
+                'School Year' => '2015',
+                'Sex' => 'Male',
+                'Nickname' => 'D_Star, The_Dawn_Star',
+                'E-Mail' => 'wlx65005@gmail.com',
+                'Blog' => ['https://github.com/wlx65003', 'http://blog.csdn.net/wlx65003', 'https://d-star.xyz'],
+                'Motto' => 'After all, tomorrow is another day.'
+            ],
+            [
+                'Name' => 'Ruiyu Zeng',
+                'Status' => 'Inactive',
+                'School Year' => '2014',
+                'Sex' => 'Male',
+                'Nickname' => 'hhhh',
+                'E-Mail' => '398585603@qq.com',
+                'Blog' => ['none'],
+                'Motto' => 'none'
+            ],
+            [
+                'Name' => 'Yupeng Chen',
+                'Status' => 'Inactive',
+                'School Year' => '2012',
+                'Sex' => 'Male',
+                'Nickname' => 'yybird, yuncity',
+                'E-Mail' => 'cyp@hsACM.com',
+                'Blog' => ['none'],
+                'Motto' => 'Who am I? Where am I? Why am I here?'
+            ]
+        ];
+
+        foreach ($people as $person) {
+            echo '<div class="box">';
+            echo '<table class="am-table">';
+            echo '<tbody>';
+            foreach ($person as $key => $value) {
+                echo '<tr>';
+                echo '<td width="20%">' . $key . '</td>';
+                if ($key == 'Name') {
+                    $badgeClass = $person['Status'] == 'Active' ? 'am-badge-success' : '';
+                    echo '<td>' . $value . ' <span class="am-badge ' . $badgeClass . '">' . $person['Status'] . '</span></td>';
+                } elseif ($key == 'Blog') {
+                    echo '<td>';
+                    foreach ($value as $link) {
+                        echo '<a href="' . $link . '">' . $link . '</a><br>';
+                    }
+                    echo '</td>';
+                } elseif ($key == 'E-Mail') {
+                    echo '<td><a href="mailto:' . $value . '">' . $value . '</a></td>';
+                } else {
+                    echo '<td>' . $value . '</td>';
+                }
+                echo '</tr>';
+            }
+            echo '</tbody>';
+            echo '</table>';
+            echo '</div>';
+        }
+        ?>
+
     </div>
 </body>
+
 </html>
 <?php include "template/hznu/footer.php" ?>
-
-
-
